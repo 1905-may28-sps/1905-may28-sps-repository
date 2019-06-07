a2dcf6897a5dd9fe627dbce91cef7ee25ec56360
Kris Lord <drkrislord@gmail.com>
2019-06-07 13:04:44 -0400

    no hardcoding allowed

--- Kris_Lord_Code/MyAssignments/JS/mix.js
+++ Kris_Lord_Code/MyAssignments/JS/mix.js
@@ -1,14 +1,51 @@
 window.onload = function () {
-    $('#liquor').on('click', 'li', function () {
-        $('.drink').append(this);})
-    $('#juice').on('click', 'li', function () {
-        $('.drink').append(this);})
-    $('#soda').on('click', 'li', function () {
-        $('.drink').append(this);})
-    $('#type').on('click', 'li', function () {
-        $('.drink').append(this);})
-//     var count = 1;
-//     $('#new').on('dblclick', function() {
-//     $(this).after("<tr><th scope='row'>" + (++count) + "</th><td><ul id='liquor'><li>Rum</li><li>Whiskey</li><li>Tequila</li><li>Cognac</li><li>Vodka</li></ul></td><td><ul id='juice'><li>orange</li><li>pineapple</li><li>cranberry</li><li>lemonade</li></ul></td><td><ul id='soda'><li>sprite</li><li>coke</li><li>ginger ale</li><li>seltzer</li></ul></td><td><ul id='type'><li>shaken</li><li>on the rocks</li><li>frozen</li><li>stirred</li><li>popscicles</li><li>jello</li></ul></td><td><ul class='drink'></ul></td><td><ul><li>cost</li><li><button class='btn-primary btn' id='order'>Order</button></li></ul></td></tr>");
-// });
-}
+    // click();
+    cost();
+    }
+    
+    // function click(){
+    // $('#liquor').on('click', 'li', drink)
+    // $('#juice').on('click', 'li', drink)
+    // $('#soda').on('click', 'li', drink)
+    // $('#type').on('click', 'li', drink)
+    // }
+
+    // function drink(){
+    //     $('.drink').append(this);
+    // }
+
+    function cost(){
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4 ){
+                var liquors = JSON.parse(xhr.responseText);
+                console.log(liquors);
+                for(let liq of liquors){
+                    var li = $(`<li id="liquor${liq.id}" 
+                        cost=${liq.cost}>
+                                ${liq.type}
+                                </li>`);
+                    //then append to list
+                }
+                    $('#liquor').append(li);
+                }
+                    //after you create list items
+                    $('#mixTable').on('click', 'li', function(){var id = $(this).attr('id'); 
+                    var cost = $(this).attr('cost');
+                    //this will be your liquor ID
+                })          
+            }
+        
+        xhr.open('GET', `http://localhost:3000/liquor`);
+        xhr.send();
+        }
+    //     xhr.open('GET', 'drinks.json', false);
+    //     xhr.onreadystatechange = function () {
+	//     if (xhr.readyState===4 && xhr.status===200) {			
+	// 	var items = JSON.parse(xhr.responseText);
+	// 	var output = ;
+	// 	document.getElementById('cost').innerHTML = output;
+	//     }
+    //     }
+    //     xhr.send();
