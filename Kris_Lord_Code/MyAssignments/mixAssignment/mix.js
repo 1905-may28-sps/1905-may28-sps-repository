8290b2fe16431679af207ac3c1a50aab9b2552fd
Kris Lord <drkrislord@gmail.com>
2019-06-10 15:04:48 -0400

    contact us page dynamic works

--- Kris_Lord_Code/MyAssignments/mixAssignment/mix.js
+++ Kris_Lord_Code/MyAssignments/mixAssignment/mix.js
@@ -13,6 +13,7 @@ window.onload = function () {
         type();
         startOver();
         order();
+        
     }
     //secret login... look for something fun
     function login() {
@@ -297,107 +298,5 @@ window.onload = function () {
 
     //testing contact page
 
-window.onload=function(){
-
-    document.getElementById('submitForm').addEventListener('click',contact);
-    
-
-}
-
-    function contact() {
-
-
-
-
-
-        var user = {
-
-            firstName: document.getElementById('firstname').value,
-
-        middleName: document.getElementById('middlename').value,
-
-        lastName: document.getElementById('lastname').value,
-
-        age: document.getElementById('age').value,
-
-        email: document.getElementById('email').value,
-
-        gender: document.getElementById('gender').value,
 
     
-
-    
-
-        }
-
-        var empty = false;
-
-        for (let prop in user) {
-
-            if (user[prop] == null || user[prop].trim() == '') {
-
-                empty = true;
-
-                document.getElementById('contactMessage').innerHTML =
-
-                    'Sorry, please fill out form completely';
-
-                // document.getElementById('register').setAttribute('disabled', "true");
-
-    
-
-            }
-
-    
-
-    
-
-        }
-
-        if (empty == false) {
-
-            //send request and clear message field
-
-            //document.getElementById('message').innerHTML = '';
-
-    
-
-            var xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = function () {
-
-                //process response
-
-    
-
-                if (xhr.readyState == 4) {
-
-                    console.log(xhr.status);
-
-                    //showLoginView();
-
-    
-
-                }
-
-            }
-
-            xhr.open('POST', 'http://localhost:3000/contact');
-
-    
-
-            xhr.setRequestHeader('content-Type', 'application/json');
-
-    
-
-            //this method is sending our user as a JSON string in POST req body
-
-            xhr.send(JSON.stringify(user));
-
-    
-
-        }
-
-        console.log(JSON.stringify(user));
-
-    }
