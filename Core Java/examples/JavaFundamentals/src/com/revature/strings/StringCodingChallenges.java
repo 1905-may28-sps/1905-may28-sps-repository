e4f3831a17983a99f05db763508bea2069a0bd1c
genesisb17 <genesis.bonds@gmail.com>
2019-06-11 13:57:26 -0400

    strings

---
+++ Core Java/examples/JavaFundamentals/src/com/revature/strings/StringCodingChallenges.java
@@ -0,0 +1,19 @@
+package com.revature.strings;
+
+public class StringCodingChallenges {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/*
+	 * Write a method that takes in a string and 
+	 * returns a boolean value true if it is a 
+	 * palindrome and a boolean value false if it is not
+	 */
+	public static boolean isPalindrome(String str) {
+		
+		return true;
+	}
+
+}
