ef0779f935b3b77e7f3c36040fe29120872e5ac0
genesisb17 <genesis.bonds@gmail.com>
2019-06-11 14:37:04 -0400

    strings and arrays

--- Core Java/examples/JavaFundamentals/src/com/revature/strings/StringCodingChallenges.java
+++ Core Java/examples/JavaFundamentals/src/com/revature/strings/StringCodingChallenges.java
@@ -3,7 +3,15 @@ package com.revature.strings;
 public class StringCodingChallenges {
 	
 	public static void main(String[] args) {
-		
+		System.out.println(testPalindrome());
+	}
+	
+	public static boolean testPalindrome() {
+		boolean test1 = isPalindrome("rAcEcAr");
+		boolean test2 = !isPalindrome("testing not");
+		boolean test3 = isPalindrome("  tt  ");
+		boolean test4 = isPalindrome("!12321!");
+		return test1 && test2 && test3 && test4;
 	}
 	
 	/*
@@ -12,8 +20,14 @@ public class StringCodingChallenges {
 	 * palindrome and a boolean value false if it is not
 	 */
 	public static boolean isPalindrome(String str) {
-		
-		return true;
+		boolean out = true;
+		for(int i = 0; i < str.length()/2; i++) {
+			String letter = "" + str.charAt(i);
+			if(!letter.equalsIgnoreCase("" + str.charAt(str.length()-1-i))) {
+				out = false;
+			}
+		}
+		return out;
 	}
 
 }
