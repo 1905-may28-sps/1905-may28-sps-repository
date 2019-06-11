146fe670c9b63c701ba5f54bb7f219fcc6ba21fb
genesisb17 <genesis.bonds@gmail.com>
2019-06-11 11:58:40 -0400

    class structure and variable scopes

--- Core Java/examples/JavaFundamentals/src/com/revature/introduction/ClassBasics.java
+++ Core Java/examples/JavaFundamentals/src/com/revature/introduction/ClassBasics.java
@@ -15,11 +15,58 @@ package com.revature.introduction; //1st line == package declaration
 import java.time.*;
 
 public class ClassBasics {
+	/*
+	 * Typically, right after the class delcaration 
+	 * we will see the declaration of our instance 
+	 * and static variables. Instance and static 
+	 * scoped variables are the only scope with 
+	 * default values (method and block scoped 
+	 * vars must be initialized to be used). 
+	 * The default value of a variable depends on 
+	 * its type
+	 */
+	String instanceVar; 
+	static String staticVar;
+	int x;
+	static int y;
 	
-	public static void main(String[] args) {
-		getCurrentTime();
+	/*
+	 * Constructor(s) usually go after the variable declarations
+	 */
+	
+	static {
+		//this is weird right! this is known as a static initializer 
+		//these blocks of code execute WHEN THE CLASS LOADS 
+		System.out.println("THIS IS A STATIC BLOCK");
 	}
 	
+	{
+		System.out.println("THIS IS AN INSTANCE BLOCK~");
+	}
+	
+	//[access mod] [non-access mod(s)] [return type*] [method name*]([params])
+	public static void main(String[] args) {
+		//we can call static methods from inside static methods 
+		getCurrentTime();
+		
+		/*
+		 * In order to call instance methods/refer to instance 
+		 * variables within static methods, we must create 
+		 * an instance of the class within itself 
+		 */
+		ClassBasics cb = new ClassBasics();
+		cb.notStatic();
+		
+	}
+	
+	//this is an instance method, because it doesnt have the word static 
+	public void notStatic() {
+		System.out.println("This is not a static method, but "
+				+ "an instance method. it can only be accessed "
+				+ "with an instance of this class");
+	}
+	
+	//this is a static method 
 	public static void getCurrentTime() {
 		/*if we dont import java.time.LocalDataTime above we 
 		/  have to refer to the class as follows
@@ -29,5 +76,9 @@ public class ClassBasics {
 		LocalDateTime currTime = LocalDateTime.now();
 		System.out.println("IT IS NOW " + currTime);
 	}
+	
+	/*
+	 * Classes can also have nested classes
+	 */
 	
 }
