a790cb0131fb21f4979288375e25f39df203807b
genesisb17 <genesis.bonds@gmail.com>
2019-06-12 14:16:31 -0400

    oop stuff

--- Core Java/examples/JavaFundamentals/src/com/revature/oop/Animal.java
+++ Core Java/examples/JavaFundamentals/src/com/revature/oop/Animal.java
@@ -5,7 +5,16 @@ package com.revature.oop;
  * the ABILITY to have abstract methods, and do not 
  * have the ability to be instantiated
  * 
- * In order to be abstract, 
+ * In order to be abstract, all we must do is use the 
+ * keyword abstract when declaring our class
+ * 
+ * abstract classes do NOT need to have abstract methods, 
+ * they simply have the ability too 
+ * 
+ * Also, weird note, abstract classes CAN have constructors 
+ * even though they cannot be instantiated
+ * 
+ * More on that here... https://www.java67.com/2013/02/can-abstract-class-have-constructor-in-java.html
  */
 public abstract class Animal implements Livable{
 
