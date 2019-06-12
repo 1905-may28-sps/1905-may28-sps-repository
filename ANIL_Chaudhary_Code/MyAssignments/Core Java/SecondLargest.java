f02985e1cb3f502ad301b70b41c3acbaa0ae1803
anilkch <ceakc@yahoo.com>
2019-06-12 12:06:48 -0400

    committing files

--- ANIL_Chaudhary_Code/MyAssignments/Core Java/SecondLargest.java
+++ ANIL_Chaudhary_Code/MyAssignments/Core Java/SecondLargest.java
@@ -4,21 +4,15 @@ package com.revature.arrays;
 import java.util.Arrays;
 
 public class SecondLargest {
-
-	  public static void main (String [] args) {
-	   int [] array = {45,12,85,32,89,39,69,44,42,1,6,8};
-	   int temp;
-	   for (int i = 1; i < array.length; i++) {
-	    for (int j = i; j > 0; j--) {
-	     if (array[j] < array [j - 1]) {
-	      temp = array[j];
-	      array[j] = array[j - 1];
-	      array[j - 1] = temp;
-	     }
-	    }
-	   }
-	   
-	     System.out.println (array[array.length-2]);
-	   }
-	  }
+public static void main (String [] args) {
 	
+	int[] nums = {1 ,5, 63, 88, 3, 9, 100, 34, 3459};
+	
+	Arrays.sort(nums);
+	
+	System.out.println("The second to largest number is " + nums[nums.length-2]);
+
+}
+
+}
+
