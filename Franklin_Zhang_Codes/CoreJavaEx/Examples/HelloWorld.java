e3c44370b18ba86cf58f37652c76fb1a81100dd9
franklinjzhang@gmail.com <franklinjzhang@gmail.com>
2019-06-12 12:32:30 -0400

    test again

---
+++ Franklin_Zhang_Codes/CoreJavaEx/Examples/HelloWorld.java
@@ -0,0 +1,24 @@
+//[access modifier] [non-access modifier(s)] class [className]{}
+public class HelloWorld{
+
+    //main method - the starting point of any non-web java app
+    public static void main(String args[]){
+
+        //print out to console
+        System.out.println("Hello World");
+    }
+
+
+
+
+
+}
+
+
+
+/* Welcome to Java! this is our first java class
+
+it is important to note that the name of the .java file must be the same name as the public class in that file
+*/
+
+//this is a single line cmment in java
