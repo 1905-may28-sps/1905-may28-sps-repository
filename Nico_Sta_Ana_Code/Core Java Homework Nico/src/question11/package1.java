31430fda58d1a20f7bd2324ff0ab416156a2dbee
neex0202 <nstaana@hotmail.com>
2019-06-19 09:42:46 -0400

    added 11

---
+++ Nico_Sta_Ana_Code/Core Java Homework Nico/src/question11/package1.java
@@ -0,0 +1,23 @@
+package question11;
+
+import question11_2.*;
+
+public class package1 {
+	
+	package_2 floats = new package_2();
+	 
+//	getX();
+	
+	
+
+
+	public static void main(String[] args) {
+		
+//		package_2 floats = new package_2();
+		
+//		getX();
+		
+
+		
+	}
+}
--- com/revature/io/SerializablePerson.java
+++
@@ -1,5 +0,0 @@
-package com.revature.io;
-
-public class Serialization {
-
-}
