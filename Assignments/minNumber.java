0669c02bd8106c2b8ec1ad631d33bbd7fb387de2
Angel <goodtimecruz@gmail.com>
2019-06-20 14:10:15 -0400

    Q10 Minimum w/ Ternary Operator Challenge
    Q10 Minimum w/ Ternary Operator Challenge

---
+++ Assignments/minNumber.java
@@ -0,0 +1,25 @@
+
+public class minNumber {
+
+	public static void main(String[] args) {
+		
+		int minNumber;
+		int maxNumber;
+		
+		int a = 55;
+		int b = 77;
+		
+		int c = 88;
+		int d = 99;
+		
+		minNumber = a < b ? a : b;
+		maxNumber = a > b ? a : b;
+		
+		System.out.println("This is the minimum number: " + minNumber);
+		System.out.println("This is the max number: " + maxNumber);
+		
+
+
+	}
+
+}
