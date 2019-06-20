dfc96fe29664b738c776c1a2cb81b099cb902a81
genesisb17 <genesis.bonds@gmail.com>
2019-06-20 15:42:16 -0400

    properties files

--- Databases/jdbc/BookstoreJDBC/src/com/revature/util/ConnectionFactory.java
+++ Databases/jdbc/BookstoreJDBC/src/com/revature/util/ConnectionFactory.java
@@ -1,8 +1,12 @@
-package com.revature.pojos;
+package com.revature.util;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 	
@@ -40,13 +44,18 @@ public class ConnectionFactory {
 	public Connection getConnection() {
 		Connection conn = null;
 		
-			String driver = "";
-			String url = "";
-			String pw = ""; 
-			String username = "";
+		Properties prop = new Properties();
+		
 			
 			//the following line of code instantiates the driver by its class name
 			try {
+				prop.load(new FileReader("db.properties"));
+				
+				String driver = prop.getProperty("driver");
+				String url = prop.getProperty("url");
+				String pw = prop.getProperty("pwd"); 
+				String username = prop.getProperty("user");
+			
 				Class.forName(driver);
 			/* The DriverManager provides a basic service for 
 			 * managing a set of JDBC drivers. As part of its 
@@ -56,6 +65,10 @@ public class ConnectionFactory {
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		
