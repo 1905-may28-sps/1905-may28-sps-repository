1246e04ce123ecdcaff710e922db5717d2ca7e5a
DHutson97 <blankempty97@gmail.com>
2019-06-20 09:07:39 -0400

    added rest of projects

---
+++ Dionne_Hutson_Code/MyAssignments/CORE JAVA/JAVA HW/ReverseString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ReverseString {
+	static String theString;
+	static String opt;
+	static Scanner scan;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter a string?");
+		scan=new Scanner(System.in);
+		theString=scan.nextLine();
+		
+		revString(theString);
+
+	}
+
+	private static void revString(String str) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
