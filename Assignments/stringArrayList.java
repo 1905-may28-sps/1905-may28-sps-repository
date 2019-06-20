d2eb3621cfa41840a34fc96efcbbde5fa05a13ce
Angel <goodtimecruz@gmail.com>
2019-06-20 16:07:48 -0400

    Q8 Str Array Palindrome Challenge
    Q8 Str Array Palindrome Challenge

---
+++ Assignments/stringArrayList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;  
+import java.util.List;  
+
+public class stringArrayList {
+
+	public static void main(String[] args) {
+		
+		 String[] names = {"Karan", "madam", "tom", "civic", "radar","sexes","jimmy","kajak","John"
+				 ,"refer","billy","did"};  
+
+	      List<String> wordList = Arrays.asList(names);  
+		
+		
+		
+//		Do palindrome logic
+		System.out.println(wordList.toString());
+		
+
+	}
+
+}
