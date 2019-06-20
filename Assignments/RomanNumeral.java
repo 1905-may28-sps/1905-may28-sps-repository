eb24957319f054391d4933d17d65e0b887233797
Angel <goodtimecruz@gmail.com>
2019-06-20 10:37:50 -0400

    Q25 number to Roman Challenge
    Q25 number to Roman Challenge incomplete

---
+++ Assignments/RomanNumeral.java
@@ -0,0 +1,41 @@
+
+public class RomanNumeral {
+
+	public static void main(String[] args) {
+//		Write a method to convert a number into its respective Roman numeral
+		
+		public String convert(int num) {
+			if(num < 1 || num > 4999)return null;
+			
+			while(num>0) {
+				if(num>1000)
+			}else if(num>500){
+				
+			}else if(num>100) {
+				
+			}else if(num>50) {
+				
+			}else if(num>10) {
+				
+			}else if(num>5) {
+				
+			}
+		}
+		@Before
+		public void setUp() thorws Exception {
+			roman = new RomanNumerals();
+		}
+		
+		@After
+		public void tearDown() throws Exception {
+			roman = null;
+		}
+		
+		@Test
+		public void testLargeNums() {
+			assertEquals("converted 3549", )
+		}
+
+	}
+
+}
