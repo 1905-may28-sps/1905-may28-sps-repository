2f096b33bf0d66d257e36ac1a9fadd5daef87ba0
Angel <goodtimecruz@gmail.com>
2019-06-20 09:32:57 -0400

    Q4 N factorial Challenge
    N factorial Challenge

---
+++ Assignments/Nfactorial.java
@@ -0,0 +1,47 @@
+
+/*
+ * 	Factorial of a number n can be defined as product of all positive numbers less than or equal to n.
+ *  It the multiplying sequence of numbers in a descending order till 1. It is defined by the symbol of exclamation (!).
+
+		1!=1
+		
+		2!=2×1
+		
+		3!=3×2×1
+		
+		4!=4×3×2×1
+		...
+		n! = n×(n−1)×(n−2)...×2×1
+ * 
+ */
+
+public class Nfactorial {
+
+	public static void main(String[] args) {
+//		use a for loop to iterate through the loop
+//		and into a switch statement which would output the factorials
+		for(int num = 0 ; num < 5; num ++) {
+			return num *= num;
+		switch (num) {
+		  case 1:
+		    System.out.println("1!" + num);
+		    break;
+		  case 2:
+		    System.out.println("2!" + num);
+		    break;
+		  case 3:
+		    System.out.println("3" + num);
+		    break;
+		  case 4:
+		    System.out.println("4" + num);
+		    break;
+		  case 5:
+		    System.out.println("5" + num);
+		    break;
+		  
+		}
+		}
+			
+	}
+
+}
