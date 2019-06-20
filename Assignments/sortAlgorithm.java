0912eeb768062d519732ad1f740b7a1080141f68
Angel <goodtimecruz@gmail.com>
2019-06-20 09:20:27 -0400

    BubbleSort Challenge
    BubbleSort Challenge

---
+++ Assignments/sortAlgorithm.java
@@ -0,0 +1,18 @@
+
+/*
+ * Q1 perform Bubblesort on the following array {1,0,5,6,3,2,3,7,9,8,4}
+ */
+public class sortAlgorithm {
+
+	public static void main(String[] args) {
+		int Bubblesort[] = {1,0,5,6,3,2,3,7,9,8,4};
+		int Length = Bubblesort.length;
+		for(int i = 0;i < Length - 1; i++ );
+			for(int x = 0; x < length-i-1; x++)
+				// error !
+				if(Bubblesort[] > Bubblesort[x+1]) {
+					
+				}
+		
+	}
+}
