7009df90a58b549da654775917ade533da0d43cb
Angel <goodtimecruz@gmail.com>
2019-06-20 13:56:11 -0400

    Q6 Even or Odd Challenge
    Q6 Even or Odd Challenge

---
+++ Assignments/EvenorOdd.java
@@ -0,0 +1,24 @@
+
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+
+			int n = 101; 
+			if(((n / 2) * 2 == n)) 
+				System.out.println( n + " This number is Even" ); 
+			else
+				System.out.println( n + " This number is Odd" );
+			
+			int n2 = 100;
+			if(((n2 / 2) * 2 == n2)) 
+				System.out.println( n2 + " This number is Even" ); 
+			else
+				System.out.println( n2 + " This number is Odd" ); 
+		} 
+	} 
+
+
+
+	
+
+
