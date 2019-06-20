ee3c5f180a35e3250356ad6375c95e997b73ccbd
Angel <goodtimecruz@gmail.com>
2019-06-20 09:18:00 -0400

    bulk upload
    bulk upload

---
+++ Assignments/HexidecimalToDecimal.js
@@ -0,0 +1,11 @@
+//convert hexidecimal into decimal 
+//grab an array of numbers then multiply it
+
+
+var Digits = ['B','C','D','9'];
+function Hexdecimal(){
+    switch
+}
+
+console.log(Digits.length)
+    
