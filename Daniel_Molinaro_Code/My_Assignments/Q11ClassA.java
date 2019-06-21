47e20eb703352ad7717c011793b15dadabe0e39e
Daniel Molinaro <danieljmolinaro@gmail.com>
2019-06-20 23:29:30 -0400

    Q11

---
+++ Daniel_Molinaro_Code/My_Assignments/Q11ClassA.java
@@ -0,0 +1,13 @@
+package corejava.revature.q11a;
+
+public class Q11ClassA {
+	
+	public static float number1 = 111.1f;
+	public static float number2 = 222.2f;
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Daniel_Molinaro_Code/My_Assignments/Q11ClassB.java
@@ -0,0 +1,13 @@
+package corejava.revature.q11b;
+
+import corejava.revature.q11a.Q11ClassA;
+
+public class Q11ClassB {
+
+	public static void main(String[] args) {
+		System.out.println(Q11ClassA.number1);
+		System.out.println(Q11ClassA.number2);
+
+	}
+
+}
