9028561df88be496059360d5b329152cb2e0302b
Jason_Lo_Revature <jasonlokamwa@gmail.com>
2019-06-20 20:17:28 -0400

    q21
    q21

---
+++ Jason_Lo_Code/My Assignment/JavaCore/src/q21/noReStr.java
@@ -0,0 +1,30 @@
+package q21;
+
+import java.util.Scanner;
+
+public class noReStr {
+		public static void main(String[] args) {
+
+			@SuppressWarnings("resource")
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter a string with some repeated characters");
+			String str = sc.nextLine();
+			System.out.println(remove(str));
+		}
+
+		public static String remove(String str) {
+	        	String theString="";
+	        	for(int i=0; i<str.length(); i++) {
+	        		if(i==0) {
+	        			theString = ""+str.charAt(i);
+	        		}else {
+	        			if(str.charAt(i-1) != str.charAt(i)) {
+	        				theString = theString + str.charAt(i);
+	        			}
+	        		}
+	        	}
+	   
+				return theString;
+		}
+
+	}
