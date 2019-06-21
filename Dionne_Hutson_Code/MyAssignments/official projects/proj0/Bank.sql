18873c8e7d05b80177ef24a8d013ef1b2ad4e372
DHutson97 <blankempty97@gmail.com>
2019-06-21 15:12:39 -0400

    add bank template

---
+++ Dionne_Hutson_Code/MyAssignments/official projects/proj0/Bank.sql
@@ -0,0 +1,32 @@
+CREATE Table Bank_User(
+--colName dataType optionalConstrains
+User_id number(10) Primary Key,
+first_name varchar2(50) NOT NULL,
+last_name varchar2(50) NOT NULL,
+ss number(10) unique Not null,
+username varchar2(50)UNIQUE NOT NULL
+);
+Create Table Bank_ACCOUNT(
+Account_id number(10) Primary Key,
+account_type varchar(8) Not Null,
+account_name varchar(50) Not Null,
+account_balance number(10,2) Not Null,
+username varchar2(50) Not Null,
+constraint FK_Account_User Foreign Key(username)
+references Bank_User(username)
+);
+
+--sequence stuff
+
+create sequence account_seq;
+commit;
+/*trigger stuff*/
+Create or replace Trigger account_Trigger
+before insert on bank_account
+for each row
+begin
+  Select account_seq.nextVal--++Genre_seq
+  into :New.account_id---:col represents a placeholder value
+  from dual;
+end;
+/
