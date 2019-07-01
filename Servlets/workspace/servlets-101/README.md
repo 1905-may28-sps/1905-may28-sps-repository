249821ece6bd4890afe5587ba26bbcb69b65292f
genesisb17 <genesis.bonds@gmail.com>
2019-07-01 15:52:09 -0400

    servlet config and servlet context

---
+++ Servlets/workspace/servlets-101/README.md
@@ -0,0 +1,11 @@
+# About this project
+
+##### Steps to recreate
+* Create Maven project packaged as .war
+* Give it a group id (usually organization site) and artifact id(whatever you want your project name to be)
+* Add tags to pom.xml and update project (right click on project --> Maven --> Update project) 
+* Generate web.xml (right click on project --> Java EE Tools --> Generate Deployment Descriptor stub)
+* Create Servlet(s) and map them in web xml 
+* Configure project on server 
+* Start server 
+* Send request
--- Servlets/workspace/servlets-101/src/main/java/com/revature/servlet/HelloServlets.java
+++ Servlets/workspace/servlets-101/src/main/java/com/revature/servlet/HelloServlets.java
@@ -19,10 +19,30 @@ import javax.servlet.ServletResponse;
  */
 public class HelloServlets extends GenericServlet{
 
+	static int counter = 0;
+	
+	/*
+	 * First method of servlet lifecycle 
+	 * here is where we initialize the servlet, potentially with 
+	 * servlet variables or special logging functionality
+	 * should always call super.init() first 
+	 * 
+	 * this init method is called by default before 
+	 * the first request to this servlet, however with the 
+	 * <load-on-startup> tag (or property if using annotations), we
+	 * can configure our servlets to be initialized upon startup 
+	 * of our container (Tomcat)
+	 */
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("INTIALIZING HELLO SERVLET");
+	}
+	
 	@Override
 	public void service(ServletRequest req, ServletResponse res) 
 			throws ServletException, IOException {
-		
+
 		/*
 		 * as we see, this service method is of void return type, 
 		 * meaning we do not RETURN our responses. we must use 
@@ -30,8 +50,26 @@ public class HelloServlets extends GenericServlet{
 		 * response object
 		 */
 		PrintWriter writer = res.getWriter();
-		writer.write("Hello, Servlet world!!!");
+		String text = "<h1>Hello, Servlet world!!!</h1>"
+				+ "Request no. "+ ++counter;
 		
+		String context = getServletContext().getInitParameter("envVar");
+		String config = getServletConfig().getInitParameter("servletVar");
+		
+		text += "<br>Servlet Context: " + context;
+		text += "<br>Servlet Config: " + config;
+		
+		res.setContentType("text/html");
+		
+		writer.write(text);
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+		System.out.println("Destroing generic servlet!");
 	}
 
 }
