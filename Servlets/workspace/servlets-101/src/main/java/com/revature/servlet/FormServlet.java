ae700436ff96a29de2a72ce8d6993a1e5150e3dd
genesisb17 <genesis.bonds@gmail.com>
2019-07-01 16:22:19 -0400

    exploring forms

--- Servlets/workspace/servlets-101/src/main/java/com/revature/servlet/FormServlet.java
+++ Servlets/workspace/servlets-101/src/main/java/com/revature/servlet/FormServlet.java
@@ -1,5 +1,24 @@
 package com.revature.servlet;
 
-public class FormServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class FormServlet extends HttpServlet{
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, 
+			HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println("do post");
+		
+	}
+
 
 }
