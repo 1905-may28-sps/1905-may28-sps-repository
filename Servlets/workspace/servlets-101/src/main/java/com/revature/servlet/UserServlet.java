ae01b748ae38a2bdb01ed6a7c387aec058ef55e6
genesisb17 <genesis.bonds@gmail.com>
2019-07-02 15:03:35 -0400

    ajax form

--- Servlets/workspace/servlets-101/src/main/java/com/revature/servlet/UserServlet.java
+++ Servlets/workspace/servlets-101/src/main/java/com/revature/servlet/UserServlet.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.pojos.User;
 import com.revature.service.DummyUserService;
 
-@WebServlet("/users")
+//@WebServlet("/users")
 public class UserServlet extends HttpServlet{
 	
 	static DummyUserService service = new DummyUserService();
