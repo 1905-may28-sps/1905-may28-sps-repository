d0a578458fbbeca385cc777dc77b10f23bbd8141
genesisb17 <genesis.bonds@gmail.com>
2019-07-08 14:33:49 -0400

    more on directives

--- ng/hello-angular/src/app/components/directives/directives.component.ts
+++ ng/hello-angular/src/app/components/directives/directives.component.ts
@@ -9,6 +9,52 @@ export class DirectivesComponent implements OnInit {
 
   showIf: boolean = true;
   numArr: number[] = [0, 1, 2, 3, 4, 5];
+  placeholder = 0;
+  buttonClass = 'btn btn-warning';
+  class = ['primary', 'secondary', 'warning', 'danger',
+  'light', 'dark'];
+  employees = [
+    {
+      name: 'Genesis',
+      role: 'Trainer'
+    },
+    {
+      name: 'Patrick',
+      role: 'Trainer'
+    },
+    {
+      name: 'Peter',
+      role: 'Trainer'
+    },
+    {
+      name: 'Carolyn',
+      role: 'Trainer'
+    },
+    {
+      name: 'Fred',
+      role: 'Trainer'
+    },
+    {
+      name: 'Daysi',
+      role: 'Associate'
+    },
+    {
+      name: 'Michael',
+      role: 'Associate'
+    },
+    {
+      name: 'Jurgen',
+      role: 'Associate'
+    },
+    {
+      name: 'Henry',
+      role: 'Associate'
+    },
+    {
+      name: 'Kevin',
+      role: 'Associate'
+    }
+  ];
 
   constructor() { }
 
