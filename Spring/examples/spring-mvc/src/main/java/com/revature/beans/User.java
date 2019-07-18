35090b294dfb09c8fb446bd0b7ae6f766eb1ff62
genesisb17 <genesis.bonds@gmail.com>
2019-07-18 16:25:27 -0400

    spring mvc

---
+++ Spring/examples/spring-mvc/src/main/java/com/revature/beans/User.java
@@ -0,0 +1,47 @@
+package com.revature.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	
+	private int id;
+	private String username;
+	private String password;
+	
+	public User() {}
+
+	public User(int id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
