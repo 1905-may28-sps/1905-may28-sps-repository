74b1769910c8bd9b0d3efb0fd709f063ec570a38
genesisb17 <genesis.bonds@gmail.com>
2019-07-18 17:03:43 -0400

    response entity

--- Spring/examples/spring-mvc/src/main/java/com/revature/controllers/UserController.java
+++ Spring/examples/spring-mvc/src/main/java/com/revature/controllers/UserController.java
@@ -1,7 +1,18 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.beans.User;
+import com.revature.service.UserService;
 
 /*
  * Now, we take a look at @RestController
@@ -18,5 +29,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	UserService service;
+	
+	
+	//GET -/user
+	@RequestMapping(method=RequestMethod.GET, 
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<User> getAllUsers(){
+		return service.getAll();
+	}
+	
+	//POST -/user
+	@RequestMapping(method=RequestMethod.POST, 
+			consumes=MediaType.APPLICATION_JSON_VALUE,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> save(@RequestBody User user){
+		//lets assume we will always have 201 status
+		user = service.save(user);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+	}
 
 }
